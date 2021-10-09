1d2fddb2ff707392bee9177fa9bfd26779147d78
Dushanka Perera <dushankadev@gmail.com>
2021-10-09 12:33:44 +0000

    Remove default comments and comment to APIs and sales worksheet

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -9,13 +6,19 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
+# APIs
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('perfumes_stock')
 
+# Daily Sales Worksheet
 daily_sales = SHEET.worksheet('daily_sales')
 sales_data = daily_sales.get_all_values()
 
 print(sales_data)
+
+
+
+
+
